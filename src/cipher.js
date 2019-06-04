b82f8850f25b8bfc1062c8f96c7c08cecca90b32
Gloria <izayura@gmail.com>
2019-06-04 16:28:01 -0500

    Cifrado arroja valor ASCII de la letra ya codificada

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,11 @@
-window.cipher = {
+    let string="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  function funcionCifrar()  {
+    var cifrado=document.getElementById("textoCifrar").value;
+
+    var espaciosCifrar=document.getElementById("caraCifrar").value;
+  document.getElementById("textoCifrado").innerHTML=(cifrado.charCodeAt()-65+parseInt(espaciosCifrar))%26+65;
+  }
+// ESTO ESTABA ANTES EN EL ARCHIVO window.cipher = {
+// }; //
   // Agregar el codigo JS para la formula que realice el cifrado//
   //con espacios a recorrer y hacia donde se aplicaria la funcion necesaria//
-};
