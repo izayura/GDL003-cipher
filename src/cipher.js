d65afbeb81af22832742decd2d7d057741f4f34a
Gloria <izayura@gmail.com>
2019-06-05 18:03:12 -0500

    Cifra en ASCII pero no arroja en letras

--- src/cipher.js
+++ src/cipher.js
@@ -1,17 +1,37 @@
-    const string="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    window.cipher = {
-    encode: funcionCifrar(string, offset)  {
-    document.getElementById("cifrar1").style.display="none";
-    document.getElementById("cifrar2").style.display="block";
-    var cifrado=document.getElementById("textoCifrar").value;
-    //si es un numero va solo el numero, si es una palabra o letra va entre ""
+    //window.cipher = {
+
+      //si es un numero va solo el numero, si es una palabra o letra va entre ""
     //asi que si esta entre "", buscar como transformarlo en numero
-    var espaciosCifrar=document.getElementById("caraCifrar").value;
-    var ascii=(cifrado.charCodeAt()-65+parseInt(espaciosCifrar))%26+65;
-    document.getElementById("textoCifrado").innerHTML=ascii.fromCharCode;
-    return "textoCifrado";
+function funcionCifrar() {
+  document.getElementById("cifrar1").style.display="none";
+  document.getElementById("cifrar2").style.display="block";
+  var cifrado=document.getElementById("textoCifrar").value;
+  var espaciosCifrar=document.getElementById("caraCifrar").value;
+  var espacio="";
+
+    for (i=0; i < cifrado.length; i++) {
+//esta funcion hace que se elijan todas las letras dentro del bucle for
+    var textAscii=cifrado.charCodeAt(i);
+    if (textAscii>=65 && textAscii<=90) {
+    var ascii=(textAscii-65+parseInt(espaciosCifrar))%26+65;
+    resultado = String.fromCharCode(ascii);
+    espacio+=ascii;
+     }
+    document.getElementById("textoCifrado").innerHTML=espacio;
       }
+    }
+
+function funcionDescifrar(){
+  document.getElementById("descifrar1").style.display="none";
+  document.getElementById("descifrar2").style.display="block";
 }
+/*<div class="contenedor">
+<input type="text" id="campoEntrada" class="entrada"/>
+    <div id="pantalla" class="salida" ></div>
+    <input type="button" value="Codificar" class="boton" onclick="codificar()" />
+    <input type="button" value="Decodificar" class="boton" onclick="decodificar()" />
+</div>*/
+
 // ESTO ESTABA ANTES EN EL ARCHIVO window.cipher = {
 // encode: funcionCifrar(string, offset)  {
   //document.getElementById("cifrar1").style.display="none";
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-  document.getElementById("bienvenida").style.display="block";
+document.getElementById("bienvenida").style.display="block";
 
 function irCifrar(){
   document.getElementById("bienvenida").style.display="none";
   document.getElementById("cifrar1").style.display="block";
 }
-
 function irDescifrar(){
   document.getElementById("bienvenida").style.display="none";
+  document.getElementById("descifrar1").style.display="block";
+}
+function irCifrar2(){
+  document.getElementById("cifrar2").style.display="none";
+  document.getElementById("descifrar2").style.display="none";
+  document.getElementById("cifrar1").style.display="block";
+}
+function irDescifrar2(){
+  document.getElementById("cifrar2").style.display="none";
+  document.getElementById("descifrar2").style.display="none";
   document.getElementById("descifrar1").style.display="block";
 }
